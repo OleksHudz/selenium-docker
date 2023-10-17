600032a9e3533718fdc802a669fe9257fe1758fe
Oleksandr Hudzovatyi <ohudzovatyi@gmail.com>
2023-10-17 18:09:16 -0400

    first commit

---
+++ src/test/java/com/project/tests/vendorportal/VendorPortalTest.java
@@ -0,0 +1,56 @@
+package com.project.tests.vendorportal;
+
+import com.project.tests.AbstractTest;
+import com.project.tests.vendorportal.model.VendorPortalTestData;
+import com.project.util.Config;
+import com.project.util.Constants;
+import com.project.util.JsonUtil;
+import com.project.vendorportal.pages.DashboardPage;
+import com.project.vendorportal.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class VendorPortalTest extends AbstractTest {
+
+    private LoginPage loginPage;
+    private DashboardPage dashboardPage;
+    private VendorPortalTestData testData;
+
+    @BeforeTest
+    @Parameters("testDataPath")
+    public void setPageObjects(String testDataPath){
+        this.loginPage = new LoginPage(driver);
+        this.dashboardPage = new DashboardPage(driver);
+        this.testData = JsonUtil.getTestData(testDataPath, VendorPortalTestData.class);
+    }
+
+    @Test
+    public void loginTest() {
+        loginPage.goTo(Config.get(Constants.VENDOR_PORTAL_URL));
+        Assert.assertTrue(loginPage.isAt());
+        loginPage.login(testData.username(), testData.password());
+    }
+
+    @Test(dependsOnMethods = "loginTest")
+    public void dashboardTest() {
+        Assert.assertTrue(dashboardPage.isAt());
+
+        // finance metrics
+        Assert.assertEquals(dashboardPage.getMonthlyEarning(), testData.monthlyEarning());
+        Assert.assertEquals(dashboardPage.getAnnualEarning(), testData.annualEarning());
+        Assert.assertEquals(dashboardPage.getProfitMargin(), testData.profitMargin());
+        Assert.assertEquals(dashboardPage.getAvailableInventory(), testData.availableInventory());
+
+        // order history search
+        dashboardPage.searchOrderHistoryBy(testData.searchKeyword());
+        Assert.assertEquals(dashboardPage.getSearchResultsCount(), testData.searchResultsCount());
+    }
+
+    @Test(dependsOnMethods = "dashboardTest")
+    public void logoutTest() {
+        dashboardPage.logout();
+        Assert.assertTrue(loginPage.isAt());
+    }
+}
